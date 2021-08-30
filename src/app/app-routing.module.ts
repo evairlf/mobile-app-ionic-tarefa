635bb2d52c593207265fab4bfa3d42261a30bdaf
evairlf <evairleitefeldmann@gmail.com>
2021-08-30 15:37:29 -0300

    primeiro commit do app de denuncias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'chat',
+    loadChildren: () => import('./chat/chat.module').then( m => m.ChatPageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: '',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
